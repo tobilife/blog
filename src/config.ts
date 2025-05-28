e55a34af402387e317869515bc4003b44764374e
tobilife <happytobilife@gmail.com>
2025-05-28 14:44:53 +0900

    fix: update profile name in config

--- src/config.ts
+++ src/config.ts
@@ -40,20 +40,20 @@ export const siteConfig: SiteConfig = {
 
 export const navBarConfig: NavBarConfig = {
 	links: [
-	 LinkPreset.Home,
-	 LinkPreset.Archive,
-	 LinkPreset.About,
-	 {
-	  name: "GitHub",
-	  url: "https://github.com/tobilife/", // Internal links should not include the base path, as it is automatically added
-	  external: true, // Show an external link icon and will open in a new tab
-	 },
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		{
+			name: "GitHub",
+			url: "https://github.com/tobilife/", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/tobilife.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "La Vita E Bella",
+	name: "토비라이프",
 	bio: "인생은 아름다워",
 	links: [
 		{
